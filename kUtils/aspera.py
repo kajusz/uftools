3e92f262414b1821da2faba79f3037f9aadd1d41
kajusz <3171277+kajusz@users.noreply.github.com>
2018-04-04 22:40:05 +0100

    kUtils, utilities for the email tool

---
+++ kUtils/aspera.py
@@ -0,0 +1,29 @@
+#!/user/bin/env python3
+
+import logging
+log = logging.getLogger(__name__)
+
+import requests
+import re
+import uuid
+import json
+
+# Returns true is added successfully
+def addDownload(link)
+    r = requests.get(link) # get the page from the link
+    assert(r.status_code == 200)
+
+    data = ''.join(str(r.text).split()) # strip new lines/spaces/tabs
+    jData = re.search('<script>transferSpec=(.*);connectSettings=.*<\/script>', data) # steal the javascript/json config that contains useful info
+    assert(jData is not None) # and fail otherwise
+
+    headers = {'Content-Type':'text/plain;charset=UTF-8', 'Origin':'https://aspera.motionpicturesolutions.net', 'Referer':link}
+    jsonData = {"transfer_specs":[{"transfer_spec":json.loads(jData.group(1)),"aspera_connect_settings":{"allow_dialogs":"no","app_id":"localhost","request_id":str(uuid.uuid4()),"back_link":link}}],"aspera_connect_settings":{"app_id":"localhost"}}
+
+    r = requests.post("https://local.connectme.us:43003/v5/connect/transfers/start", headers=headers, json=jsonData)
+    assert(r.status_code == 200)
+    if not (r.status_code == requests.codes.ok):
+        print(r.status_code, r.reason)
+        return False
+    else:
+        return True
---
+++ kUtils/mailSearch.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import logging
+log = logging.getLogger(__name__)
+
+import email
+import re
+import os
+
+# returns a tuple with the link, and trailer title
+def findTrailerLink(message):
+    msg = email.message_from_bytes(message)
+
+    subject = re.search('Delivery via DigiLink: (.*)$', msg['Subject'])
+    if subject == None:
+        log.warn('Message subject "%s" is not valid.', msg['Subject'])
+
+    subject = str(subject.group(1)) # get trailer title
+
+    if msg.is_multipart():
+        for part in msg.get_payload():
+            message = part.get_payload(decode=True)
+            if part.get_content_type() == 'text/html': # want the html message with <a href=...
+                link = re.search(r".*Click to download the trailer via Aspera: <br \/><a href=.?'(https.*)\\'>.*", str(message), re.MULTILINE)
+                if link == None:
+                    log.warn('Could not find a link in "%s".', subject)
+                    continue
+                link = link.group(1)
+
+                return (link, subject) # add to the downloader
+
+        # We should not be in this function any more if we have successfully found the link
+        log.error('Failed to find a link in message "%s".', subject)
+        return False
+    else:
+        # All messages from MPS appear to be multipart plain + html and html is easier to dodgily parse
+        log.critical('WOW, a non multipart mesasge, someone needs to fix this!!!')
+        return False
+
+# returns array of downloaded files
+def findAndDownloadKeys(message, downloadDir='/tmp/'):
+    ret = []
+
+    msg = email.message_from_bytes(message)
+    subject = msg['Subject']
+    log.debug('Looking at "%s"', subject)
+
+    for part in msg.walk():
+        if part.get_content_maintype() == 'multipart':
+            continue
+        if part.get('Content-Disposition') is None:
+            continue
+
+        filename = part.get_filename()
+        log.debug('---> found "%s"', filename)
+        dlPath = os.path.join(downloadDir, filename)
+
+        if not os.path.isfile(dlPath) :
+            fp = open(dlPath, 'wb')
+            ret.append(dlPath)
+            fp.write(part.get_payload(decode=True))
+            fp.close()
+        else:
+            raise IOError('File %s exists' % (dlPath))
+
+    return ret
